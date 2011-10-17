7498dd372ef4e2f164ca822273bff39b91bed4d0
Putera Fajar <libputera@gmail.com>
2011-10-17 23:14:48 +0700

    Improved configs

--- index-dev.php
+++ index-dev.php
@@ -1,8 +1,12 @@
 <?php
+/**
+ * This is the bootstrap file for test application.
+ * This file should be removed when the application is deployed for production.
+ */
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/prod.php';
+$config=dirname(__FILE__).'/protected/config/dev.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
