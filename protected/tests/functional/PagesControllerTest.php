cea72432beef8fbd4424a10e58579ab0b5aa4f70
Putera Fajar <libputera@gmail.com>
2011-10-18 22:56:09 +0700

    Added functional test with EHttpClient extension

---
+++ protected/tests/functional/PagesControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+Yii::import('ext.httpclient.*');
+Yii::import('ext.httpclient.adapter.*');
+
+class PagesControllerTest extends PHPUnit_Framework_TestCase {
+
+  public function testHome() {
+    $client = new EHttpClient(TEST_BASE_URL.'/pages/home');
+    $this->assertTrue($client->request()->isSuccessful());
+  }
+
+  public function testContact() {
+    $client = new EHttpClient(TEST_BASE_URL.'/pages/contact');
+    $this->assertTrue($client->request()->isSuccessful());
+  }
+
+  public function testError() {
+    $client = new EHttpClient(TEST_BASE_URL.'/pages/error-not-found');
+    $this->assertTrue($client->request()->isError());
+  }
+
+  public function testLoginLogout() {
+    $client = new EHttpClient(TEST_BASE_URL.'/site/login');
+    $client->setCookieJar();
+    $this->assertTrue($client->request()->isSuccessful());
+    $this->assertRegExp('/Login/', $client->request('POST')->getBody());
+
+    $client->resetParameters();
+    $client->setParameterPost(array(
+      'LoginForm[username]' => '',
+      'LoginForm[password]' => ''
+    ));
+    $this->assertRegExp('/Username cannot be blank./', $client->request('POST')->getBody());
+    $this->assertRegExp('/Password cannot be blank./', $client->request('POST')->getBody());
+
+    $client->resetParameters();
+    $client->setParameterPost(array(
+      'LoginForm[username]' => 'wronguser',
+      'LoginForm[password]' => 'wrongpass'
+    ));
+    $this->assertRegExp('/Incorrect username or password./', $client->request('POST')->getBody());
+
+    $client->resetParameters();
+    $client->setParameterPost(array(
+      'LoginForm[username]' => 'demo',
+      'LoginForm[password]' => 'demo'
+    ));
+    $this->assertNotRegExp('/Username cannot be blank./', $client->request('POST')->getBody());
+    $this->assertNotRegExp('/Password cannot be blank./', $client->request('POST')->getBody());
+
+    $client->setUri(TEST_BASE_URL);
+    $this->assertTrue($client->request()->isSuccessful());
+    $this->assertRegExp('/Logout \(demo\)/', $client->request('POST')->getBody());
+
+    $client->setUri(TEST_BASE_URL.'/site/logout');
+    $this->assertTrue($client->request()->isSuccessful());
+
+    $client->setUri(TEST_BASE_URL);
+    $this->assertNotRegExp('/Logout \(demo\)/', $client->request('POST')->getBody());
+  }
+
+}
--- protected/views/pages/home.php
+++ protected/views/pages/home.php
@@ -1,11 +1,7 @@
 <?php
-$this->breadcrumbs=array(
-	'Pages'=>array('/pages'),
-	'Home',
-);?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+$this->breadcrumbs = array(
+  'Pages' => array('/pages'),
+  'Home',
+);
+?>
+<h1><?php echo $this->id.'/'.$this->action->id; ?></h1>
