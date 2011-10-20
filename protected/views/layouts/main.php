98051fbf09d9b5e995b8007aef96c10908631515
Putera Fajar <libputera@gmail.com>
2011-10-20 21:37:56 +0700

    require is a language construct, not a function

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,15 +2,15 @@
 <html>
   <head>
     <title><?php echo CHtml::encode(Helper::title($this->pageTitle)); ?></title>
-    <?php require('_stylesheets.php'); ?>
+    <?php require '_stylesheets.php'; ?>
   </head>
   <body>
     <div class="container">
-      <?php require('_header.php'); ?>
+      <?php require '_header.php'; ?>
       <section class="round">
         <?php echo $content; ?>
       </section>
     </div>
-    <?php require('_footer.php'); ?>
+    <?php require '_footer.php'; ?>
   </body>
 </html>
